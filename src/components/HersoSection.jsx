b975f0dff6e5b857b0c9fecd1a4e1e4f36536d48
kamran_jamal <kamrankhanwxr@gmail.com>
2025-01-10 16:55:56 +0500

    hero section done

---
+++ src/components/HersoSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HersoSection() {
+  return (
+    <div className="w-full h-[80vh] bg-[#0a192f] p-4 flex justify-center flex-col items-center text-gray-300">
+      <h1 className="text-7xl font-extrabold ">Simple Blogging App</h1>
+      <p className="mt-4 text-2xl font-semibold text-center">
+        This blogging app contain the daily basis life information like
+        cricket,news food cooking, programming and much more techs info.
+      </p>
+    </div>
+  );
+}
+
+export default HersoSection;
