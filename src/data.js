597b81e3f9b638b8af225183c55bfd34a585f3c3
kamran_jamal <kamrankhanwxr@gmail.com>
2025-01-10 16:47:37 +0500

    inital commit

---
+++ src/data.js
@@ -0,0 +1,93 @@
+[
+    {
+      "id": 1,
+      "title": "Top 10 JavaScript Frameworks in 2025",
+      "description": "Explore the best JavaScript frameworks to watch for in 2025, including insights into their features and use cases.",
+      "img": "https://img.freepik.com/free-photo/ai-nuclear-energy-future-innovation-disruptive-technology_53876-129784.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Technology",
+      "author": "John Doe",
+      "date": "2025-01-05"
+    },
+    {
+      "id": 2,
+      "title": "The Ultimate Guide to Healthy Eating",
+      "description": "Learn how to maintain a balanced diet with this comprehensive guide on healthy eating.",
+      "img": "https://img.freepik.com/free-photo/portrait-smiley-female-health-worker_23-2148980799.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Health",
+      "author": "Jane Smith",
+      "date": "2024-12-20"
+    },
+    {
+      "id": 3,
+      "title": "Top Travel Destinations for 2025",
+      "description": "Discover the most beautiful travel destinations to visit in 2025.",
+      "img": "https://img.freepik.com/premium-photo/rear-view-woman-standing-by-rock-against-sky_1048944-6483471.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Travel",
+      "author": "Michael Brown",
+      "date": "2025-01-02"
+    },
+    {
+      "id": 4,
+      "title": "Beginner's Guide to Investing",
+      "description": "Everything you need to know to start your investment journey.",
+      "img": "https://img.freepik.com/free-photo/unrecognizable-business-partners-working-with-statistic-charts-businessman-holding-tablet-professional-content-businesswoman-making-notes-statistics-communication-partnership-concept_74855-14268.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Finance",
+      "author": "Emily Davis",
+      "date": "2025-01-07"
+    },
+    {
+      "id": 5,
+      "title": "Mastering React in 2025",
+      "description": "Get ahead with this comprehensive guide to React in 2025.",
+      "img": "https://img.freepik.com/free-vector/code-typing-concept-illustration_114360-3581.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Programming",
+      "author": "Chris Johnson",
+      "date": "2025-01-08"
+    },
+    {
+      "id": 6,
+      "title": "10 Easy Home Workouts",
+      "description": "Stay fit with these simple and effective home workout routines.",
+      "img": "https://img.freepik.com/free-photo/side-view-athlete-holding-weights_23-2148398830.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Fitness",
+      "author": "Sarah Lee",
+      "date": "2024-12-15"
+    },
+    {
+      "id": 7,
+      "title": "The Future of Artificial Intelligence",
+      "description": "How AI is shaping the world in 2025 and beyond.",
+      "img": "https://img.freepik.com/premium-photo/person-using-augmented-reality-device_23-2150794785.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Technology",
+      "author": "Alan Turing",
+      "date": "2025-01-04"
+    },
+    {
+      "id": 8,
+      "title": "How to Bake Perfect Bread at Home",
+      "description": "Step-by-step guide to baking delicious bread from scratch.",
+      "img": "https://img.freepik.com/free-photo/chicken-skewers-with-slices-apples-chili_2829-19992.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Food",
+      "author": "Baker Joe",
+      "date": "2024-12-30"
+    },
+    {
+      "id": 9,
+      "title": "Top Fashion Trends for 2025",
+      "description": "Stay ahead of the curve with these top fashion trends.",
+      "img": "https://img.freepik.com/free-photo/portrait-young-beautiful-hipster-bad-girl-trendy-red-summer-hoodie-earring-her-nose-sexy-carefree-smiling-blond-woman-posing-studio-wig-positive-model-licking-round-sugar-candy_158538-15573.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Fashion",
+      "author": "Mia Taylor",
+      "date": "2025-01-01"
+    },
+    {
+      "id": 10,
+      "title": "The Basics of Cryptocurrency",
+      "description": "A beginner's guide to understanding and using cryptocurrency.",
+      "img": "https://img.freepik.com/free-photo/loan-mortgage-payment-property-concept_53876-124868.jpg?uid=R179283473&ga=GA1.1.244016193.1733721289&semt=ais_hybrid",
+      "category": "Finance",
+      "author": "David Miller",
+      "date": "2024-12-29"
+    }
+  ]
+    
